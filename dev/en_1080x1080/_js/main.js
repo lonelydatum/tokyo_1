7476b21d7f0240e1af3c1303320e0603d162b0ad
Gar <gar@lonelydatum.com>
2021-06-25 14:34:15 -0400

    ON

---
+++ dev/en_1080x1080/_js/main.js
@@ -0,0 +1,28 @@
+import {size, frameEnd, init, read} from '../../_common/js/common.js'
+import {player} from '../../_common/js/striker.js'
+
+
+function start(){
+	
+	// return
+	const tl = init(6)
+
+	
+	tl.from(".high", .3, {opacity:0})
+
+	tl.from(".t1", .3, {opacity:0}, "+=.5")
+	tl.to(".t1", .3, {opacity:0}, `+=${read.t1}`)
+	tl.from(".t2", .3, {opacity:0})
+
+	tl.from(".cta", .3, {opacity:0, y:"+=30"}, "+=.5")
+
+// CustomBounce.create("myBounce", {strength:0.7, squash:3});
+
+}
+
+
+start()
+
+
+module.exports = {};
+
